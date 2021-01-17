f38c707ed0930704bc0bc0ab8fa44b064349b0ff
Sandrine Ngo <sandrinengo@yahoo.com>
2021-01-17 09:15:51 -0500

    initial commit

---
+++ MicroRabbit.Domain.Core/Bus/IEventHandler.cs
@@ -0,0 +1,20 @@
+﻿using MicroRabbit.Domain.Core.Events;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MicroRabbit.Domain.Core.Bus
+{
+    //This event handler takes any type of event
+    public interface IEventHandler<in TEvent> : IEventHandler
+        where TEvent : Event//and with restriction where the event that coming in has to be type of Event
+    {
+        Task Handle(TEvent @event);
+    }
+
+    public interface IEventHandler
+    {
+
+    }
+}
